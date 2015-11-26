4b7aad669334b289a3279f49cf9b846657544e9c
mjaszczak <mjaszczak@sigma.ug.edu.pl>
2015-11-26 21:31:11 +0100

    Implementacja mark_sort

---
+++ Zadanie2/lib/mark_sort.rb
@@ -0,0 +1,22 @@
+
+  def mark_sort(array)
+    array_max = array.max
+    array_min = array.min
+    markings = [0] * (array_max - array_min + 1)
+    array.each do |a|
+      markings[a - array_min] += 1
+    end
+
+    res = []
+    markings.length.times do |i|
+      markings[i].times do
+        res << i + array_min;
+      end
+    end
+
+    res
+  end
+  #arr = [9, 2, 3, 4, 5, 6, 7, 8]
+ # arr = mark_sort(arr)
+  #p mark_sort(arr)
+ # puts arr
---
+++ Zadanie2/spec/my_example_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require_relative '../lib/mark_sort'
+
+describe 'Test Mark_sort class' do
+
+  before :each do
+    @tab = []
+    expect(@tab).to be_empty
+    @tab = [5,3,6,2,6,9,1,2]
+  end
+  it 'should do something' do
+    
+
+  end
+end
